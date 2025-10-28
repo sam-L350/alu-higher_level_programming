541291d273e59937944f0893151c77444e6e702f
sam-L350 <s.nizeyiman@alustudent.com>
2025-10-28 05:40:40 -0700

    done

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """
-    Returns a new list where all occurrences of 'search' are replaced with 'replace'.
-    The original list is not modified.
+    Return a new list where all occurrences of 'search' are replaced
+    with 'replace'. The original list is not modified.
     """
     new_list = [replace if x == search else x for x in my_list]
     return new_list
