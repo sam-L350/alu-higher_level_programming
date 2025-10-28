eedaf77e7e4b5a4a2b094ebf59827aba52feee56
sam-L350 <s.nizeyiman@alustudent.com>
2025-10-28 03:28:28 -0700

    done

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Return a new list with element replaced at idx without modifying the original list."""
+    """
+    Return a new list with element replaced at idx without modifying
+    the original list.
+    """
     new_list = my_list.copy()
     if idx < 0 or idx >= len(new_list):
         return new_list
