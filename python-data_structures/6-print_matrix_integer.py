e3437baaa54f06e04e135e0eea33e2f33c6c0646
sam-L350 <s.nizeyiman@alustudent.com>
2025-10-28 03:38:31 -0700

    done

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers, one row per line, values separated by spaces."""
+    """
+    Print a matrix of integers, one row per line, values separated by spaces.
+    """
     for row in matrix:
-        print(" ".join(
-            "{:d}".format(num) for num in row
-        ))
+        print(" ".join("{:d}".format(num) for num in row))
+
